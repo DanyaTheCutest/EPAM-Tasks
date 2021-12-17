fe544362aacd6ea34c155ed1eddf23d295ed7cc2
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-17 23:07:45 +0400

    AnotherTriangle

---
+++ Task 1/Task 1.1/Task 1.1.3/AnotherTriangle/AnotherTriangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AnotherTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawTriangle();
+        }
+        static void DrawTriangle()
+        {
+            int n;
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            } while (!Int32.TryParse(input, out n));
+            Console.Clear();
+            
+            int spaces;
+            int stars;
+            for (int i = n; i >= 0; i--)
+            {
+                stars = 2 * n - 2 * i + 1;
+                spaces = i;
+                while (spaces >= 0)
+                {
+                    Console.Write(" ");
+                    spaces--;
+                }
+                while (stars > 0)
+                {
+                    Console.Write("*");
+                    if (stars == 1)
+                        Console.WriteLine();
+                    stars--;
+                }                
+            }
+        }
+    }
+}
