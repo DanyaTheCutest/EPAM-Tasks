279d3f59ea0a3b45d20c82fb9b302cf66a041aea
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-17 22:15:06 +0400

    Triangle done

--- Task 1/Task 1.1/Task 1.1.2/Triangle/Triangle/Program.cs
+++ Task 1/Task 1.1/Task 1.1.2/Triangle/Triangle/Program.cs
@@ -6,11 +6,28 @@ namespace Triangle
     {
         static void Main(string[] args)
         {
-            
+            DrawTriangle();
         }
         static void DrawTriangle()
         {
-            
+            int number;
+            string input = "";
+            do
+            {
+               input = Console.ReadLine();
+            } while (!Int32.TryParse(input, out number));
+            Console.Clear();
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                    if (j == i - 1)
+                        Console.WriteLine("");
+                }
+            }
+
         }
     }
 }
