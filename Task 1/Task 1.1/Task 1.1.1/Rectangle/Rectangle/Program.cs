9c513128b68f3b8f63aa0135d38a5dd0109e39c4
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-17 21:48:40 +0400

    Task 1.1.1 complete

---
+++ Task 1/Task 1.1/Task 1.1.1/Rectangle/Rectangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetArea();
+        }
+
+        static void GetArea()
+        {
+            int number;
+            string input = "";
+
+            do
+            {
+                Console.Write("Введите значение a: ");
+                input = Console.ReadLine();
+            } while (!Int32.TryParse(input, out number));
+            int a = number;
+            do
+            {
+                Console.Write("Введите значение b: ");
+                input = Console.ReadLine();
+            } while (!Int32.TryParse(input, out number));
+            int b = number;
+            Console.WriteLine($"Ответ: {a * b}");
+        }
+    }
+}
