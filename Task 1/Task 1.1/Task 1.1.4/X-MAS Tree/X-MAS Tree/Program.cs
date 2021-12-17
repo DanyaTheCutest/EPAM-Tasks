64ae4417b8c2cfb9e8587775fe253f7d86ff63f2
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-18 00:55:59 +0400

    X mas

---
+++ Task 1/Task 1.1/Task 1.1.4/X-MAS Tree/X-MAS Tree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace X_MAS_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            } while (!Int32.TryParse(input, out n));
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    string branch = new String('*', j);
+                    Console.WriteLine(branch.PadLeft(n + 3) + "*" + branch);
+                }
+            }
+        }                                 
+    }
+}
