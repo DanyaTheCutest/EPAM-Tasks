4e2081a554e2080fa14457f4549c7b6d996deff6
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-21 17:16:43 +0400

    1.1.8 done

---
+++ Task 1/Task 1.1/Task 1.1.8/No Positive/No Positive/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace No_Positive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = CreateRandomArray();
+            arr = ReplacePositives(arr);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int[,,] ReplacePositives(int[,,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arr.GetLength(2); k++)
+                    {
+                        if (arr[i, j, k] > 0)
+                            arr[i, j, k] = 0;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        static int[,,] CreateRandomArray()
+        {
+            Random r = new Random();
+            var arr = new int[5, 5, 5];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arr.GetLength(2); k++)
+                    {
+                        arr[i, j, k] = r.Next(-100, 100);
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
