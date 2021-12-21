7b7d82169c2ac092c71264f817491c5274b1ebf3
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-22 02:30:28 +0400

    1.2.2 started...

--- Task 1/Task 1.2/Task 1.2.2/Program.cs
+++ Task 1/Task 1.2/Task 1.2.2/Program.cs
@@ -6,7 +6,16 @@ namespace Task_1._2._2
     {
         static void Main(string[] args)
         {
-            
+            string inputString = Console.ReadLine();
+            string doublerString = Console.ReadLine();
+
+            Console.WriteLine(Doubler(inputString, doublerString));
+        }
+        static string Doubler(string inputString, string doublerString)
+        {
+            //TODO
+
+            return null;
         }
     }
 }
