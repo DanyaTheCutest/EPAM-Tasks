7d8046607bd5aaab3ebbf102a31981b63d5cadf4
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-21 17:22:46 +0400

    1.1.9 Done

---
+++ Task 1/Task 1.1/Task 1.1.9/Non-Negative Sum/Non-Negative Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Non_Negative_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = CreateRandomArray();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Сумма неотрицательных элементов: {NonNegativeSum(arr)}");
+        }
+
+        static int NonNegativeSum(int[] arr)
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                if (item >= 0)
+                    sum += item;
+            }
+
+            return sum;
+        }
+
+        static int[] CreateRandomArray()
+        {
+            Random r = new Random();
+            var arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(-100, 100);
+            }
+            return arr;
+        }
+    }
+}
