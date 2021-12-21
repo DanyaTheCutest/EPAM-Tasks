82141d235472ca54b2306f4f5d93b334f40ad01a
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-22 01:46:50 +0400

    1.2.1 Done

---
+++ Task 1/Task 1.2/Averages/Averages/Task 1.1.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Averages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            string input = Console.ReadLine();
+            Console.WriteLine(GetAverage(input)); //Результат округлён в большую сторону
+        }
+        static double GetAverage(string str)
+        {            
+            double symbolsNumber = 0;
+            double wordsNumber = 0;
+            str = str.Trim();
+            var arr = str.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != arr.Length - 1)
+                {
+                    if (Char.IsLetterOrDigit(arr[i]))
+                        symbolsNumber++;
+                    else if (Char.IsWhiteSpace(arr[i]))
+                        wordsNumber++;
+                }
+                else
+                {
+                    wordsNumber++;
+                    symbolsNumber++;
+                }
+            }
+            
+            return Math.Ceiling(symbolsNumber / wordsNumber);
+        }
+    }
+}
