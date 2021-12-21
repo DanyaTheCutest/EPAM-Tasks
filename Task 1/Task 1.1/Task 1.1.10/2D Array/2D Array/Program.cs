da394fb9ba9043b66ccddc97f8d2013d9e9f8c52
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-21 17:35:11 +0400

    2D Array Done

---
+++ Task 1/Task 1.1/Task 1.1.10/2D Array/2D Array/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _2D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = CreateRandomArray(2, 3);
+            PrintArray(arr);
+            Console.WriteLine();
+
+            Console.WriteLine($"Сумма элементов, стоящих на чётных позициях: {FindEvenSum(arr)}");
+        }
+
+        static int FindEvenSum(int[,] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (i + j % 2 == 0)
+                        sum += arr[i, j];
+                }
+            }
+
+            return sum;
+        }
+
+        static int[,] CreateRandomArray(int a, int b)
+        {
+            Random r = new Random();
+            var arr = new int[a, b];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                   arr[i,j] = r.Next(-10, 15);
+                }
+            }
+            return arr;
+        }
+        static void PrintArray(int [,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
