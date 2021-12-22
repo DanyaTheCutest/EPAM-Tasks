158f04fa10b03c1128a0129d9c5a7a46d6615596
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-22 17:59:40 +0400

    1.2.3 done

---
+++ Task 1/Task 1.2/Task 1.2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_1._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(CountLower(input));
+        }
+        static int CountLower(string input)
+        {
+            int count = 0;
+            var arr = input.ToCharArray();
+            if (Char.IsLower(arr[0]))
+                count++;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (Char.IsLower(arr[i]) && Char.IsLetterOrDigit(arr[i-1]) == false)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
