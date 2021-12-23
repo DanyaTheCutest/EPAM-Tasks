53c3a8defd56add6b5c6eabd6423f2d75b7f9bf0
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-23 23:35:44 +0400

    1.1.2 divided one methods into 1, removed extra loop

--- Task 1/Task 1.1/Task 1.1.2/Triangle/Triangle/Program.cs
+++ Task 1/Task 1.1/Task 1.1.2/Triangle/Triangle/Program.cs
@@ -6,28 +6,33 @@ namespace Triangle
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите число N: ");
             DrawTriangle();
         }
         static void DrawTriangle()
         {
-            int number;
-            string input = "";
-            do
-            {
-               input = Console.ReadLine();
-            } while (!Int32.TryParse(input, out number));
+            var number = ReadInt();
             Console.Clear();
 
             for (int i = 1; i <= number; i++)
             {
                 for (int j = 0; j < i; j++)
                 {
-                    Console.Write("*");
-                    if (j == i - 1)
-                        Console.WriteLine("");
+                    Console.Write("*");                   
                 }
+                Console.WriteLine();
             }
 
+        }
+        static int ReadInt()
+        {
+            int number;
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+            } while (!Int32.TryParse(input, out number));
+            return number;
         }
     }
 }
