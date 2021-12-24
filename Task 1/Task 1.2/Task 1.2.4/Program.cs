66472b6a7aa038cfddd102b83d15d374a368774b
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-24 14:52:26 +0400

    1.2.4 Methods renamed

--- Task 1/Task 1.2/Task 1.2.4/Program.cs
+++ Task 1/Task 1.2/Task 1.2.4/Program.cs
@@ -10,7 +10,7 @@ namespace Task_1._2._4
             string input = Console.ReadLine();
             Console.WriteLine($"Результат: {Validator(input)}");
         }
-        static string Validator(string input)
+        static string Validate(string input)
         {
             StringBuilder result = new StringBuilder("");
             var arr = input.ToCharArray();
@@ -18,14 +18,14 @@ namespace Task_1._2._4
             result.Append(arr[1]);
             for (int i = 2; i < arr.Length; i++)
             {
-                if (isEndMark(arr[i-2]) && Char.IsLetter(arr[i]))
+                if (IsEndMark(arr[i-2]) && Char.IsLetter(arr[i]))
                     result.Append(Char.ToUpper(arr[i]));
                 else result.Append(arr[i]);
             }
 
             return result.ToString();
         }
-        static bool isEndMark(char c) =>
+        static bool IsEndMark(char c) =>
             c == '.' || c == '?' || c == '!';                           
     }
 }
