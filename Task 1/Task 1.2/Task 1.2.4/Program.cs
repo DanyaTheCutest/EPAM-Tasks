7a3938423c892847cc9e69545df0c3d700826d16
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2021-12-24 13:53:13 +0400

    1.2.4 replaced base string with StringBuilder

--- Task 1/Task 1.2/Task 1.2.4/Program.cs
+++ Task 1/Task 1.2/Task 1.2.4/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text;
 namespace Task_1._2._4
 {
     class Program
@@ -12,18 +12,18 @@ namespace Task_1._2._4
         }
         static string Validator(string input)
         {
-            string result = "";
+            StringBuilder result = new StringBuilder("");
             var arr = input.ToCharArray();
-            result += Char.ToUpper(arr[0]).ToString();
-            result += arr[1].ToString();
+            result.Append(Char.ToUpper(arr[0]).ToString());
+            result.Append(arr[1].ToString());
             for (int i = 2; i < arr.Length; i++)
             {
                 if (isEndMark(arr[i-2]) && Char.IsLetter(arr[i]))
-                    result += Char.ToUpper(arr[i]).ToString();
-                else result += arr[i].ToString();
+                    result.Append(Char.ToUpper(arr[i]).ToString());
+                else result.Append(arr[i].ToString());
             }
 
-            return result;
+            return result.ToString();
         }
         static bool isEndMark(char c) =>
             c == '.' || c == '?' || c == '!';
