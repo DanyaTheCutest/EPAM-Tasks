8220697f006a92b340f1ac6007e937a3628730b4
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2022-01-05 20:29:33 +0400

    2.1.1 basic methods added

--- Task 2/Task 2.1/Task 2.1.1/MyOwnString.cs
+++ Task 2/Task 2.1/Task 2.1.1/MyOwnString.cs
@@ -8,6 +8,69 @@ namespace Task_2._1._1
 {
     public class MyOwnString
     {
+        public char[] superString;
+        
 
+        public MyOwnString(char[] superString)
+        {
+            this.superString = superString;
+        }
+        
+        public bool IsEqual(char[] string_1, char[] string_2)
+        {
+            if (string_1.Length != string_2.Length)
+                return false;
+            for (int i = 0; i < string_1.Length; i++)
+            {
+                if (string_1[i] != string_2[i])
+                    return false;
+            }
+            return true;
+        }
+
+        public void ConcatStrings(char c)
+        {
+            char[] result = new char[superString.Length + 1];
+            superString.CopyTo(result, 0);
+            result[result.Length - 1] = c;
+            superString = result;
+        }
+        public void ConcatStrings(MyOwnString anotherString)
+        {
+            char[] result = new char[superString.Length + anotherString.superString.Length];
+            superString.CopyTo(result, 0);
+            anotherString.superString.CopyTo(result, superString.Length);
+            superString = result;
+            
+        }
+
+        public int FindSymbolIndex(char c)
+        {
+             if (superString.Contains(c))
+             {
+                for (int i = 0; i < superString.Length; i++)
+                {
+                    if (superString[i] == c)
+                        return i;
+                }
+             }
+            return -1;
+        }
+        public void PrintString()
+        {
+            for (int i = 0; i < superString.Length; i++)
+            {
+                Console.Write(superString[i]);
+            }           
+        }
+
+        public char[] ConvertToString()
+        {
+            Console.Write("Enter a string: ");
+            return Console.ReadLine().ToCharArray();
+        }
+
+        public int ConvertFromString(MyOwnString convert) => 
+            Convert.ToInt32(convert.superString.ToString());
     }
 }
--- Task 2/Task 2.1/Task 2.1.1/Program.cs
+++ Task 2/Task 2.1/Task 2.1.1/Program.cs
@@ -6,7 +6,17 @@ namespace Task_2._1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            var mySuperString = new char[11] { 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd' };
+            var mySuperString_2 = new char[1] { 'a' };
+
+            MyOwnString myString = new MyOwnString(mySuperString);
+            MyOwnString myString_2 = new MyOwnString(mySuperString_2);
+
+            myString.ConcatStrings(myString_2);
+            Console.WriteLine(myString.FindSymbolIndex('w'));
+            myString.superString = myString.ConvertToString();
+            myString.PrintString();
+
         }
     }
 }
