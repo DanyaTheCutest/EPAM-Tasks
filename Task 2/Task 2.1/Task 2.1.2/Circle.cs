94fed57c43c928e6530ff0f0592c8c9ff081b7e2
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2022-01-06 17:15:24 +0400

    Ring and Circle added

---
+++ Task 2/Task 2.1/Task 2.1.2/Circle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1._2
+{
+    public class Circle
+    {
+        private double _centerX;
+        private double _centerY;
+        private double _radius;
+        private const double _pi = Math.PI;
+        private double _area;
+        private double _roundLength;
+
+        public double Radius
+        {
+            get { return _radius; }
+            set 
+            {
+                if (value > 0)
+                    _radius = value; 
+            }
+        }
+        public double X
+        {
+            get { return _centerX; }
+            set { _centerX = value; }
+        }
+        public double Y
+        {
+            get { return _centerY; }
+            set { _centerY = value; }
+        }
+
+
+        public Circle(double x, double y, double r)
+        {
+            X = x;
+            Y = y;
+            Radius = r;
+        }
+        public Circle()
+        {
+
+        }
+
+        public double GetArea()
+        {
+            _area = 2 * _pi * (_radius * _radius);
+            return _area;
+        }
+
+        public double GetLength()
+        {
+            _roundLength = 2 * _pi * _radius;
+            return _roundLength;
+        }
+        
+    }
+}
---
+++ Task 2/Task 2.1/Task 2.1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_2._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle(2.0, 1.0, 3);
+            Console.WriteLine(circle.GetArea()); 
+        }
+    }
+}
