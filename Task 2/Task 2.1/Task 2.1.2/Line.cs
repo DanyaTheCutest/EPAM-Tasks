2e7a4c7c10345589d80a5d432e232ae837e98095
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2022-01-08 20:15:01 +0400

    Basic classes added

---
+++ Task 2/Task 2.1/Task 2.1.2/Line.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1._2
+{
+    public class Line : Point
+    {
+        private Point _startPoint;
+        private Point _endPoint;
+        private double _length;
+
+        
+        
+        public Line(Point start, Point end) 
+        {
+            _startPoint = start;
+            _endPoint = end;
+            SetLength(start, end);
+        }
+
+        
+        private void SetLength(Point start, Point end) =>
+            _length = end.CoordinateX - start.CoordinateX + end.CoordinateY - start.CoordinateY;
+
+
+
+
+        public double Length
+        {
+            get => _length;
+        }
+    }
+}
---
+++ Task 2/Task 2.1/Task 2.1.2/Ring.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1._2
+{
+    public class Ring : Circle
+    {
+        Circle InnerCircle = new Circle();
+        Circle OuterCircle = new Circle();
+        private double _ringArea;
+        private double _ringLength;
+
+        public Ring(double x, double y, double innerRadius, double outerRadius)
+        {
+            InnerCircle.X = x;
+            OuterCircle.X = x;
+            InnerCircle.Y = y;
+            OuterCircle.Y = y;
+            InnerCircle.Radius = innerRadius;
+            OuterCircle.Radius = outerRadius;
+        }
+        public double GetRingArea()
+        {
+            _ringArea = Math.PI *
+                (OuterCircle.Radius * OuterCircle.Radius - InnerCircle.Radius * InnerCircle.Radius);
+
+            return _ringArea;
+        }
+
+        public double GetRingLength()
+        {
+            _ringLength = OuterCircle.GetLength() + InnerCircle.GetLength();
+
+            return _ringLength;
+        }
+    }
+}
