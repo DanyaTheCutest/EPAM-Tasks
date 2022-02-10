81aeec344f935efec190b94be7722f8619977988
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2022-02-11 02:06:21 +0400

    3.2 methods added

---
+++ Task 3/Task 3.2/Task 3.2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new DynamicArray<int>();
+            arr.Add(5);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
