cb24f16ce7984ac47c7e633ff0deb0e1efa0f2b8
DanyaTheCutest <vlasyuk.danil@yandex.ru>
2022-02-13 15:56:41 +0400

    3.3.1 done

---
+++ Task 3/Task 3.3/Task 3.3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Task_3._3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 3, 3, 4, 5 };
+
+            Console.WriteLine(arr.FindAverage());
+            Console.WriteLine(arr.FindSum());
+            Console.WriteLine(arr.FindMostCommon());
+
+            arr = arr.ApplyToMass((x) => x * x);
+            Console.WriteLine();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Task 3/Task 3.3/Task 3.3.1/SuperArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._3._1
+{
+    public static class SuperArray
+    {
+        public static int FindSum(this int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        
+        public static int FindAverage(this int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum / arr.Count();
+        }
+
+        public static int FindMostCommon(this int[] arr)
+        {
+            return arr.GroupBy(x => x).OrderByDescending(g => g.Count()).First().Key;
+        }
+
+        public static int[] ApplyToMass(this int[] arr, Func<int, int> func)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = func(arr[i]);
+            }
+            return arr;
+        }
+    }
+}
